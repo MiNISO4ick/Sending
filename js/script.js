819f7588bdaa619759fd86ceda40d5aba660c92c
MiNISO4ick <89798859+MiNISO4ick@users.noreply.github.com>
2022-01-23 22:15:06 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-/*fetch('https://jsonplaceholder.typicode.com/photos').then(response => response.json()).then(json => console.log(json.splice(0,10)))
-
-async function getResponse(){
-   let response = await fetch('https://jsonplaceholder.typicode.com/photos')
-   let content = await response.json()
-   content = content.splice(0,10)
-
-   let list = document.querySelector(".list");
-   let key;
-   for (key in content) {
-      list.innerHTML += `
-         <h1>${content[key].title}</h1>
-         <img src="${content[key].url}" alt="">
-      `
-      console.log(content[key]);
-   }
-}
-
-getResponse()
-*/
 "use strict"
 document.addEventListener('DOMContentLoaded',function() {
    const form = document.querySelector("#form");
